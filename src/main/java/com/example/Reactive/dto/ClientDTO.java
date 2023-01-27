9382a2e6a04cc67aee33bdf1f3d4a9049a21a177
Renos Bardis <renosbardis@gmail.com>
2023-01-27 20:50:15 +0200

    feature: Adding DTO layer and mapper

---
+++ src/main/java/com/example/Reactive/dto/ClientDTO.java
@@ -0,0 +1,15 @@
+package com.example.Reactive.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientDTO {
+
+    private String name;
+    private String surName;
+    private String email;
+}
